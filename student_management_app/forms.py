2ad8a9b372c55f478f0d299e60af48190b100893
Eddie694 <edisonemerenu@gmail.com>
2023-09-11 18:58:56 +0100

    Adjust of the add subject

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -20,42 +20,20 @@ class SessionForm(forms.Form):
            'session_end_year':DateInput(),
         }
     
-class SubjectForm(forms.Form):
-    
-    #For Displaying Courses
-    try:
-        courses = Course.objects.all()
-        course_list = []
-        for course in courses:
-            single_course = (course.id, course.course_name)  # Corrected attribute name
-            course_list.append(single_course)
-            print(course_list)
-            
-    except Course.DoesNotExist:
-        print('No courses found')
-        course_list = []
-        
-   
-    
-    #For Displaying list of Teachers
-    try:
-        staff_users = CustomUser.objects.filter(user_type=CustomUser.STAFF)  # Filter users by user type
-        print(f"Number of staff users found: {staff_users.count()}")
-        print(staff_users)
-        staff_list = []
-        
-        for staff_user in staff_users:
-            single_staff = (staff_user.id, f"{staff_user.first_name} {staff_user.last_name}")
-            staff_list.append(single_staff)
-        if not staff_list:
-            print("No staff members found")
-    except CustomUser.DoesNotExist:
-        print("An error occurred while retrieving staff members")
 
-    
-    subject_name = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'form-control' }))
-    course_id = forms.ChoiceField( choices=course_list, required=True, widget = forms.Select(attrs={'class': "form-control"}))
-    staff_id = forms.ChoiceField( choices = staff_list, required=True, widget=forms.Select(attrs={'class':'form-control'}))
+class SubjectForm(forms.ModelForm):
+    subject_name = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    course_id = forms.ChoiceField(required=True, widget=forms.Select(attrs={'class': 'form-control'}))
+    staff_id = forms.ChoiceField(required=True, widget=forms.Select(attrs={'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Dynamically populate the staff_id choices with (user_id, full_name) tuples
+        course_choices = [(course.id, f"{course.course_name}") for course in Course.objects.all()]
+        staff_choices = [(staff.id, f"{staff.first_name} {staff.last_name}") for staff in CustomUser.objects.filter(user_type=CustomUser.STAFF)]
+        self.fields['course_id'].choices = course_choices
+        self.fields['staff_id'].choices = staff_choices
     
     class Meta:
         model = Subject
